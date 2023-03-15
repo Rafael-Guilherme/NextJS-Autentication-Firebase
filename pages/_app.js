cba2c5dda234dd9898bc9b044afad427fceacb11
Rafael <rafael_guilherme@hotmai.com>
2023-03-15 15:09:41 -0300

    changes auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import { AuthContextProvider } from "@/contexts/AuthContext"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  )
 }
+
+export default App
